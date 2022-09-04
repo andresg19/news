4cc5e9bf051054d09d3a8c470c856ce8abaa513f
andresg19 <andygermain17@gmail.com>
2022-09-04 20:10:35 -0300

    navbar and sections controlados al no tener usuario

--- client/src/Components/NavBar/Nav.jsx
+++ client/src/Components/NavBar/Nav.jsx
@@ -13,10 +13,18 @@ import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 // import AdbIcon from '@mui/icons-material/Adb';
 import swal from "sweetalert";
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const pages = ['News', 'Curiosities'];
 
 const ResponsiveAppBar = () => {
+  const [user, setUset] = useState(
+    JSON.parse(localStorage.getItem("actualUser"))
+    ? JSON.parse(localStorage.getItem("actualUser"))
+    : [],
+  );
+  let navigate = useNavigate();
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   
 
@@ -28,6 +36,10 @@ const ResponsiveAppBar = () => {
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
+
+  const handleLogIn = () => {
+    navigate('/Login');
+  }
   
   const handleLogOut = () => {
     swal({
@@ -53,6 +65,9 @@ const ResponsiveAppBar = () => {
   }
 
 
+
+if( user.length === 0) {
+
   return (
     <AppBar position="static">
       <Container maxWidth="xl" className="tool">
@@ -75,7 +90,7 @@ const ResponsiveAppBar = () => {
           >
             LOGO
           </Typography>
-
+          
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
@@ -105,11 +120,132 @@ const ResponsiveAppBar = () => {
                 display: { xs: "block", md: "none" },
               }}
             >
-              {pages.map((page) => (
+              {/* {pages.map((page) => (
                 <MenuItem key={page} onClick={handleCloseNavMenu}>
                   <Typography textAlign="center">{page}</Typography>
                 </MenuItem>
-              ))}
+              ))} */}
+
+              <Button
+                onClick={handleLogIn}
+                sx={{ my: 2, color: "red", display: "block" }}
+              >
+                log in
+              </Button>
+            </Menu>
+          </Box>
+          {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} /> */}
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "red",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {/* {pages.map((page) => (
+              
+              <Button
+                key={page}
+                href={`/${page}`}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "red", display: "block" }}
+              >
+                {page}
+              </Button>
+              
+            ))} */}
+            <Button
+                onClick={handleLogIn}
+                sx={{ my: 2, color: "red", display: "block" }}
+              >
+                log in
+              </Button>
+          </Box>
+          {/* <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <Button
+              onClick={handleLogOut}
+              sx={{ my: 2, color: "red", display: "block" }}
+            >
+              log out
+            </Button>
+          </Box> */}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+
+} else {
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl" className="tool">
+        <Toolbar disableGutters>
+          {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "red",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+          
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+             <Button href='/News' sx={{ my: 2, color: "red", display: "block" }}>
+              News
+             </Button>
+
+             <Button href='/Curiosities' sx={{ my: 2, color: "red", display: "block" }}>
+              Curiosities
+             </Button>
 
               <Button
                 onClick={handleLogOut}
@@ -139,18 +275,15 @@ const ResponsiveAppBar = () => {
             LOGO
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              
-              <Button
-                key={page}
-                href={`/${page}`}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "red", display: "block" }}
-              >
-                {page}
-              </Button>
-              
-            ))}
+           
+          <Button href='/News' sx={{ my: 2, color: "red", display: "block" }}>
+              News
+             </Button>
+
+             <Button href='/Curiosities' sx={{ my: 2, color: "red", display: "block" }}>
+              Curiosities
+             </Button>
+
             <Button
                 onClick={handleLogOut}
                 sx={{ my: 2, color: "red", display: "block" }}
@@ -158,17 +291,12 @@ const ResponsiveAppBar = () => {
                 log out
               </Button>
           </Box>
-          {/* <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            <Button
-              onClick={handleLogOut}
-              sx={{ my: 2, color: "red", display: "block" }}
-            >
-              log out
-            </Button>
-          </Box> */}
         </Toolbar>
       </Container>
     </AppBar>
   );
+
+} 
+
 };
 export default ResponsiveAppBar;
